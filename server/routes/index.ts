89ad9cb3942d3dffd803e0f369e7ca3017f0d3d2
Matthew Kerry <matthew.kerry@digital.justice.gov.uk>
2024-02-23 15:53:42 +0000

    Revert "Add some temp logging to confirm dev is logging (#6)"
    This reverts commit c9a3949024ef2ed0673a6c9ad207fb25d3c76095.

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -2,7 +2,6 @@ import { type RequestHandler, Router } from 'express'
 
 import asyncMiddleware from '../middleware/asyncMiddleware'
 import type { Services } from '../services'
-import logger from '../../logger'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function routes(service: Services): Router {
@@ -10,7 +9,6 @@ export default function routes(service: Services): Router {
   const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
 
   get('/', (req, res, next) => {
-    logger.debug('Get Index')
     res.render('pages/index')
   })
 
